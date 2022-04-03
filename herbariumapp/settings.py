26a4840e8efdae73d049c567f07849ac62a75d11
Bahonika <bahonika1@mail.ru>
2022-04-03 11:49:31 +0300

    django with flutter together

--- herbariumapp/settings.py
+++ herbariumapp/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/herbarium/baginskiy/static/'
-MEDIA_URL = '/herbarium/baginskiy/media/'
+STATIC_URL = '/herbarium/static/'
+MEDIA_URL = '/herbarium/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
