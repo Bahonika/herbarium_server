b588bbab450f3923e430965fc96258f3214fd0a6
Bahonika <bahonika1@mail.ru>
2022-04-04 20:57:37 +0300

    Repaired models without image_urls

--- herbarium/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-# class PlantImageCreate(generics.CreateAPIView):
-#     serializer_class = serializers.PlantImageSerializer
-#     queryset = PlantImage.objects.all()
