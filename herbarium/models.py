cfbd67d2a2f630933eedb8c198d9a9711039fb1a
Bahonika <bahonika1@mail.ru>
2022-05-22 11:29:52 +0300

    small images v1

--- herbarium/models.py
+++ herbarium/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
+from os import path
 
 class Plant(models.Model):
     serial_number = models.IntegerField(null=None, default=None)
@@ -13,6 +14,29 @@ class Plant(models.Model):
     collector = models.CharField(max_length=150)
     determinate = models.CharField(max_length=150)
     photo = models.ImageField(upload_to='images/%Y/%m/%d/')
+    photo_s = models.ImageField(upload_to='images/%Y/%m/%d/s/'),
+
+    def save(self, *args, **kwargs):
+        # Сначала - обычное сохранение
+        super(Plant, self).save(*args, **kwargs)
+
+        # Проверяем, указан ли логотип
+        if self.photo:
+            filepath = self.photo.path
+            width = self.photo.width
+            height = self.photo.height
+            max_size = max(width, height)
+
+            photo = Image.open(filepath)
+            # resize - безопасная функция, она создаёт новый объект, а не
+            photo_s = photo.resize(
+                (round(width / max_size * 250),  # Сохраняем пропорции
+                 round(height / max_size * 250)),
+            )
+            # И не забыть сохраниться
+            new_filepath = filepath[0: len(filepath)-4] + "s.jpg"
+            print(new_filepath)
+            photo_s.save(new_filepath)
 
 
 class Subscriber(models.Model):
